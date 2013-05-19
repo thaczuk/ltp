3d9d825cffa45bf828c97f216e7f3c67d414d44f
thaczuk <thaczuk@gmail.com>
2013-05-19 06:49:19 -0600

    up to ch5

---
+++ ch5/biggerBetterNumber.rb
@@ -0,0 +1,4 @@
+puts 'What is your favorite number?'
+number = gets.chomp.to_i + 1
+
+puts number.to_s + ' is a bigger, better number than yours!'
---
+++ ch5/fullNameGreeting.rb
@@ -0,0 +1,10 @@
+puts 'What is your first name?'
+firstName = gets.chomp
+
+puts 'What is your middle name?'
+middleName = gets.chomp
+
+puts 'What is your last name?'
+lastName = gets.chomp
+
+puts 'Nice to meet you ' + firstName + ' ' + middleName + ' ' + lastName
